c1d58597434d7118c6401054b425f9d9e9dcc933
DeblaDAW <145448999+DeblaDAW@users.noreply.github.com>
2024-05-17 14:00:25 +0200

    Spring Boot
    Perdon Mariona no me di cuenta y elegi mal la carpeta asique comiteo ahora todo

---
+++ Angular Eclipse/gestion-escuelas-backend/src/test/java/com/gestion/escuelas/GestionEscuelasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.escuelas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEscuelasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
